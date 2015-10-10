c80903a52a1d1036242257a76278126a785799a5
Jacob Ebey <jacob.ebey@live.com>
2015-10-10 02:04:57 -0700

    Update for CI Server.

---
+++ NuGet/pack.ps1
@@ -0,0 +1,13 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\IO\bin\Release\ZimCode.IO.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\ZimCode.IO.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\NuGet\ZimCode.IO.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\NuGet\ZimCode.IO.compiled.nuspec
+
